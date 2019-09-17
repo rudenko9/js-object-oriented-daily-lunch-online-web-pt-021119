e2940cc008bd121d9b1dbfe54951e3d55ffb6c7f
Nataliya R <n.rudenko@gmail.com>
2019-09-17 20:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighbId = 1;
+let neighborhoodIdId = 1;
 class Neighborhood {
   constructor(name) {
-    this.id = ++neighbId
+    this.id = ++neighborhoodId
     this.name = name 
     
     store.neighborhoods.push(this)
